c82b81dec75128e9936188465364a64e23803aff
BohdanKonovalenko <bodiakonovalenko@gmail.com>
2022-12-28 13:03:42 +0200

    fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='demo',
-    version='1.0.1',
+    version='1.0.2',
     python_requires='>=3.6',
     install_requires=requirements,
     include_package_data=True,
